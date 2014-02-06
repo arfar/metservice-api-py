c32affcc417d1737876142fb051e4cb9eeb168f3
Arthur Roberts <arthur.roberts@alliedtelesis.co.nz>
2014-02-07 09:13:49 +1300

    Working now
    Only tested with Christchurch. Will need to test with other towns

--- metservice.py
+++ metservice.py
@@ -1,5 +1,37 @@
 import urllib2
-
+import sys
+import json
+import pprint
 
 METSERVICE_BASE = 'http://metservice.com/publicData/'
-SUN_PROTECTION_URL = 'sunProtectionAlert'
+# It's a little bit ugly, but it allows me to iterate over them later
+API_OPTIONS = {
+    'LOCAL_FORECAST': 'localForecast',
+    'SUN_PROTECTION_URL': 'sunProtectionAlert',
+    'ONE_MIN_OBS': 'oneMinObs_',
+    'HOURLY_OBS_AND_FORCAST': 'hourlyObsAndForecast_',
+    'LOCAL_OBS': 'localObs_',
+    'TIDES': 'tides_',
+    'WARNINGS': 'warningsForRegion3_urban.',
+    'RISE_TIMES': 'riseSet_',
+    'POLLEN_LEVELS': 'pollen_town_',
+    'DAILY_FORECAST': 'climateDataDailyTown_{0}_32',
+}
+
+
+def get_response(url):
+    response = urllib2.urlopen(url)
+    return json.loads(response.read())
+
+if __name__ == '__main__':
+    try:
+        city = sys.argv[1]
+    except IndexError:
+        city = 'christchurch'
+
+    for key in API_OPTIONS.iterkeys():
+        if key == 'DAILY_FORECAST':
+            url = ''.join([METSERVICE_BASE, API_OPTIONS[key].format(city)])
+        else:
+            url = ''.join([METSERVICE_BASE, API_OPTIONS[key], city])
+        pprint.pprint(get_response(url))
